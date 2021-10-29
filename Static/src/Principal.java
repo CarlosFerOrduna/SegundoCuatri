6cce87dc88aac3d8dff9852e6387aed745b92faa
CarlosFerOrduna <fernandoorduna.co@gmail.com>
2021-10-29 10:40:37 -0300

    Practica de clase estatica

---
+++ Static/src/Principal.java
@@ -0,0 +1,11 @@
+import utilities.Calculadora;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		System.out.println(Calculadora.multiplicar(5, 5));
+
+	}
+
+}
---
+++ Static/src/utilities/Calculadora.java
@@ -0,0 +1,28 @@
+package utilities;
+
+public class Calculadora {
+	
+	public static double sumar(double primerNumero, double segundoNumero) {
+		
+		double resultado = 0;
+		
+		resultado = primerNumero + segundoNumero;
+		
+		return resultado;
+	}
+	public static double restar(double primerNumero, double segundoNumero) {
+		double resultado = 0;
+		
+		resultado = primerNumero - segundoNumero;
+		
+		return resultado;
+	}
+	public static double multiplicar(double primerNumero, double segundoNumero) {
+		double resultado = 0;
+		
+		resultado = primerNumero * segundoNumero;
+		
+		return resultado;
+		
+	}
+}
